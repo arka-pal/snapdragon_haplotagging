c7225ddbc85456f8e367b909d5266067f78d96d0
Arka Pal <arka.pal@ist.ac.at>
2024-06-07 18:48:59 -0700

    Modified recRate

--- recRate/_scripts/job-LDhat.sbatch.sh
+++ recRate/_scripts/job-LDhat.sbatch.sh
@@ -22,8 +22,8 @@
 #SBATCH --time=240:00:00
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
 
 
 #SBATCH --mail-user=arka.pal@ist.ac.at
